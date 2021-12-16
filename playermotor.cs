30d366d396e3bb0806a1f1c4368800681dbd47b4
Batool-ba2255 <96236409+Batool-ba2255@users.noreply.github.com>
2021-12-16 15:05:44 +0400

    Update playermotor.cs

--- playermotor.cs
+++ playermotor.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class playermotor : MonoBehaviour
 {
-
-private CharacterController controller;
-private float speed = 5.0f;
-    // Start is called before the first frame update
+    public Slider mySlider;
+    private CharacterController controller;
+    private float speed = 5.0f;
     void Start()
     {
-       controller = GetComponent<CharacterController> ();
-       
+        mySlider.onValueChanged.AddListener(delegate{ValueChangeCheck();});
+        speed = mySlider.value*15;
+        controller = GetComponent<CharacterController> ();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        controller.Move ((Vector3.forward * speed) * Time.deltaTime);
+        controller.Move ((Vector3.forward * speed) *
+        Time.deltaTime);
     }
+    public void ValueChangeCheck(){
+    Debug.Log(mySlider.value);
+    speed = mySlider.value*15;}
 }
