cdb246aed5047b7b1f03eaea20f048997b7f66ce
Rayray28992 <96233342+Rayray28992@users.noreply.github.com>
2021-12-16 15:06:58 +0400

    Add files via upload

--- playermotor.cs
+++ playermotor.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class playermotor : MonoBehaviour
 {
-    public Slider mySlider;
-    private CharacterController controller;
-    private float speed = 5.0f;
+        public Slider mySlider;
+private CharacterController controller;
+private float speed = 15.0f;
+    // Start is called before the first frame update
     void Start()
     {
         mySlider.onValueChanged.AddListener(delegate{ValueChangeCheck();});
-        speed = mySlider.value*15;
-        controller = GetComponent<CharacterController> ();
+        speed = mySlider.value*7;
+       controller = GetComponent<CharacterController> ();
+       
     }
+
+    // Update is called once per frame
     void Update()
     {
-        controller.Move ((Vector3.forward * speed) *
-        Time.deltaTime);
+        controller.Move ((Vector3.forward * speed) * Time.deltaTime);
     }
+    
+    
     public void ValueChangeCheck(){
     Debug.Log(mySlider.value);
-    speed = mySlider.value*15;}
+    speed = mySlider.value*7;
+}
 }
